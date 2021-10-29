5e0a3ac789dc951f29c68055a54ed237bd449f24
statisticallyfit <statisticallyfit@gmail.com>
2021-10-30 00:29:38 +0300

    gamma cdf added - not understanding why values aren't as expected. Need to use cdf-splining drawing code from eriklanderson

--- src/main/scala/Project_IsarnSketches/Example_TDigestSketch.scala
+++ src/main/scala/Project_IsarnSketches/Example_TDigestSketch.scala
@@ -4,6 +4,8 @@ package Project_IsarnSketches
  *
  */
 
+import breeze.stats.distributions.Gamma
+import breeze.stats.distributions.Rand.FixedSeed.randBasis
 import org.isarnproject.sketches.TDigest
 
 
@@ -14,7 +16,7 @@ object Example_TDigestSketch extends App {
 	val sketch: TDigest = TDigest.sketch(data)
 	// the cumulative distribution function of the sketch; cdf(x) at x = 0
 	val cdf: Double = sketch.cdf(0.0)
-	sketch.samplePDF
+
 	// inverse of the CDF, evaluated at q = 0.5
 	val cdfi: Double = sketch.cdfInverse(0.5) //should be close to 0, since mean(gaussian) == 0
 
@@ -23,5 +25,37 @@ object Example_TDigestSketch extends App {
 	println(s"cdfi = $cdfi")
 	// ----------------------------------------------------------
 
-	//val gammaData: Vector[Double] =
+	val gamm = new Gamma(shape = 2, scale = 2) //long right tail of the pdf, right-skewed
+	val gammaData: Seq[Double] = gamm.sample(10000)
+	// sketch cdf of the gamma data
+	val gammaSketch: TDigest = TDigest.sketch(gammaData)
+	// the cdf of the sketch; cdf(x) at x = 0
+	val cdf0: Double = sketch.cdf(0)
+	// inverse of cdf sketch at q = 0.5
+	println("\nGamma skewed right:")
+	println(s"cdf0 = $cdf0") // 0
+	println(s"cdf(1) = ${gammaSketch.cdf(1)}") // 0.5
+	println(s"cdf(2) = ${gammaSketch.cdf(2)}") // 0.88
+	println(s"cdf(3) = ${gammaSketch.cdf(3)}") // 0.99
+	println(s"cdf(4) = ${gammaSketch.cdf(4)}") // 1.00
+
+
+	println(s"cdf inverse(0.5) = ${gammaSketch.cdfInverse(0.5)}")
+	println(s"cdf inverse(0.3) = ${gammaSketch.cdfInverse(0.3)}")
+
+	// -----
+	val gammaDataLocalized: Seq[Double] = Gamma(shape = 15, scale = 3).sample(10000)
+	val gammaSketchLocalized: TDigest = TDigest.sketch(gammaDataLocalized)
+	println("\nGamma more normal-looking:")
+	println(s"cdf(0) = ${gammaSketchLocalized.cdf(0)}")
+	println(s"cdf(1) = ${gammaSketchLocalized.cdf(1)}")
+	println(s"cdf(2) = ${gammaSketchLocalized.cdf(2)}")
+	println(s"cdf(3) = ${gammaSketchLocalized.cdf(3)}")
+	println(s"cdf(4) = ${gammaSketchLocalized.cdf(4)}")
+	println(s"cdf(7) = ${gammaSketchLocalized.cdf(7)}")
+	println(s"cdf(10) = ${gammaSketchLocalized.cdf(10)}")
+
+
+	println(s"cdf inverse(0.5) = ${gammaSketchLocalized.cdfInverse(0.5)}")
+	println(s"cdf inverse(0.3) = ${gammaSketchLocalized.cdfInverse(0.3)}")
 }
