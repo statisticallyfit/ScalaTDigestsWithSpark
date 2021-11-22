d57cc4b442304655ee4408c3fc5a62350bd2fa26
statisticallyfit <statisticallyfit@gmail.com>
2021-11-22 14:26:04 +0200

    finally found the wrong definition of tdigest combine to properly compare it on graph with the good definition of tdigest combine

--- src/main/scala/Project_IsarnSketchesAlgebirdAPI/AlgebirdFactory.scala
+++ src/main/scala/Project_IsarnSketchesAlgebirdAPI/AlgebirdFactory.scala
@@ -19,14 +19,14 @@ import org.json4s.JObject
 import util.GeneralUtil
 
 import scala.collection.immutable
-
+import scala.util.Random
 import com.cibo.evilplot._
+import com.cibo.evilplot.colors.Clear.opacity
 import com.cibo.evilplot.plot._
 import com.cibo.evilplot.plot.renderers.BoxRenderer
 import com.cibo.evilplot.plot.aesthetics.DefaultTheme._
 import com.cibo.evilplot.colors.Color
-import com.cibo.evilplot.colors.HTMLNamedColors.{dodgerBlue, fireBrick}
-
+import com.cibo.evilplot.colors.HTMLNamedColors.{dodgerBlue, fireBrick, red}
 import com.cibo.evilplot.geometry.{Drawable, Extent, Rect}
 
 /**
@@ -89,10 +89,28 @@ object experiment {
 	type Indices = Seq[Int]
 
 
-	def combine(ltd: TDigest, rtd: TDigest, delta: Double = TDigest.deltaDefault): TDigest = {
+
+
+
+	def algoCombineSort(ltd: TDigest, rtd: TDigest, delta: Double = TDigest.deltaDefault): TDigest = {
+
+		// insert clusters from largest to smallest, instead of randomly
+		(ltd.clusters.toVector ++ rtd.clusters.toVector).sortWith((a, b) => a._2 > b._2)
+			.foldLeft(TDigest.empty(delta))((d, e) => d + e)
+	}
+
+	def algoCombineRandom(ltd: TDigest, rtd: TDigest, delta: Double = TDigest.deltaDefault): TDigest = {
+		// randomly shuffle input clusters and re-insert to a new t-digest
+		Random.shuffle(ltd.clusters.toVector ++ rtd.clusters.toVector)
+			.foldLeft(TDigest.empty(delta))((d, e) => d + e)
+	}
+
+	def combine(ltd: TDigest, rtd: TDigest,
+			  GOOD_DEF: Boolean = true,
+			  delta: Double = TDigest.deltaDefault): TDigest = {
 
 		if (ltd.nclusters <= 1 && rtd.nclusters > 1) {
-			combine(rtd, ltd, delta)
+			combine(rtd, ltd, GOOD_DEF, delta)
 		}
 		else if (rtd.nclusters == 0) {
 			ltd
@@ -105,27 +123,26 @@ object experiment {
 			ltd + ((d.key, d.value))
 
 		} else {
-			// insert clusters from largest to smallest, instead of randomly
-			(ltd.clusters.toVector ++ rtd.clusters.toVector).sortWith((a, b) => a._2 > b._2)
-				.foldLeft(TDigest.empty(delta))((d, e) => d + e)
+
+			// NOTE: comparing good and bad definitions here.
+			GOOD_DEF match {
+				case true => algoCombineSort(ltd, rtd, delta)
+				case false => algoCombineRandom(ltd, rtd, delta)
+			}
 		}
 	}
 
-	def combineRandom = {
-		def combine(ltd: TDigest, rtd: TDigest): TDigest = {
-			// randomly shuffle input clusters and re-insert to a new t-digest
-			shuffle(ltd.clusters.toVector ++ rtd.clusters.toVector)
-				.foldLeft(TDigest.empty)((d, e) => d + e)
-		}
-	}
 
 	def monoid: Monoid[TDigest] =	new Monoid[TDigest] {
 
 			def zero: TDigest = TDigest.empty(TDigest.deltaDefault)
 
-			// TODO what is the delta here?error if say x.delta as in original code: https://github.com/erikerlandson/isarn-sketches-algebird-api/blob/blog/t_digest_sum/src/main/scala/org/isarnproject/sketchesAlgebirdAPI/AlgebirdFactory.scala#L91
 			def plus(x: TDigest, y: TDigest/*, delta: Double*/): TDigest = combine(x, y)
+
+		// TODO what is the delta here?error if say x.delta as in original code: https://github.com/erikerlandson/isarn-sketches-algebird-api/blob/blog/t_digest_sum/src/main/scala/org/isarnproject/sketchesAlgebirdAPI/AlgebirdFactory.scala#L91
 		}
+
+
 
 	// Kolmogorov-Smirnov D statistic
 	def kolmogorovSmirnovDStatistic(td: TDigest, dist: RealDistribution): Double = {
@@ -137,40 +154,47 @@ object experiment {
 		d
 	}
 
+
+
+
+	final val SAMPLE_SIZE: Int = 10
+	final val DATA_SIZE: Int = 10000
+	final val N_SUMS: Int = 100
+	final val SUM_SAMPLE: Int = 50 // 10
+
+
 	def collect(mon: Monoid[TDigest], dist: RealDistribution)/*: (BrokenTDigestAdd, OrderedTDigestAdd, Indices)*/ = {
-		val sampleSize: Int = 10
-		val dataSize: Int = 10000
-		val nsums: Int = 100
-		val sumSample: Int = 50 // 10
 
+		val raw: Vector[(BrokenTDigestAdd, OrderedTDigestAdd)] = Vector.fill(SAMPLE_SIZE) {
+			val data: Vector[Vector[Double]] = Vector.fill(1 + N_SUMS) { Vector.fill(DATA_SIZE) { dist.sample } }
 
-		val raw: Vector[(BrokenTDigestAdd, OrderedTDigestAdd)] = Vector.fill(sampleSize) {
-			val data: Vector[Vector[Double]] = Vector.fill(1 + nsums) { Vector.fill(dataSize) { dist.sample } }
-
-			// NOTE: monoidal addition as defined in the original paper
-			val ref: BrokenTDigestAdd = data
+			// NOTE: monoidal addition as defined in the original paper (bad def)
+			val oldDef: BrokenTDigestAdd = data
 				.map((distSample: Vector[Double]) => TDigest.sketch(distSample)) //vec of tdigests
-				.scanLeft(TDigest.empty())((ltd: TDigest, rtd: TDigest) => TDigest.combine(ltd, rtd))
+				.scanLeft(TDigest.empty())((ltd: TDigest, rtd: TDigest) =>
+					combine(ltd, rtd, GOOD_DEF = false))
 				.drop(1)
 				.map((tdigest: TDigest) => kolmogorovSmirnovDStatistic(tdigest, dist)) // vector of doubles KSDs
 
 			// NOTE: experimental definition where clusters are inserted from largest to smallest
-			val exp: OrderedTDigestAdd = data
-				.map(TDigest.sketch(_))
-				.scanLeft(TDigest.empty())(combine(_, _)).drop(1)
-				.map(kolmogorovSmirnovDStatistic(_, dist)) // vector
+			val fixedDef: OrderedTDigestAdd = data
+				.map((distSample: Vector[Double]) => TDigest.sketch(distSample)) //vec of tdigests
+				.scanLeft(TDigest.empty())((ltd: TDigest, rtd: TDigest) =>
+					combine(ltd, rtd, GOOD_DEF = true))
+				.drop(1)
+				.map((tdigest: TDigest) => kolmogorovSmirnovDStatistic(tdigest, dist)) // vector
 
-			(ref, exp)
+			(oldDef, fixedDef)
 		}
 
-		val step: Int = math.max(1, nsums / sumSample)
-		val jvals: Range = 0 to nsums by step // pick out by num monoidal additions
-		val ref: Seq[Seq[Double]] = jvals.map(j => raw.map(_._1(j))) ///jvals.flatMap(j => raw.map(_._1(j)))
-		val exp: Seq[Seq[Double]] = jvals.map(j => raw.map(_._2(j))) //jvals.flatMap(j => raw.map(_._2(j)))
-		val jvf: Seq[Seq[Int]] = jvals.map(j => Vector.fill(sampleSize)(j)) //axis? indices? //jvals.flatMap(j =>
+		val step: Int = math.max(1, N_SUMS / SUM_SAMPLE)
+		val jvals: Range = 0 to N_SUMS by step // pick out by num monoidal additions
+		val oldDef: Seq[Seq[Double]] = jvals.map(j => raw.map(_._1(j))) ///jvals.flatMap(j => raw.map(_._1(j)))
+		val fixedDef: Seq[Seq[Double]] = jvals.map(j => raw.map(_._2(j))) //jvals.flatMap(j => raw.map(_._2(j)))
+		val indices: Seq[Seq[Int]] = jvals.map(j => Vector.fill(SAMPLE_SIZE)(j)) //axis? indices? //jvals.flatMap(j =>
 		// Vector.fill(sampleSize)(j)) //axis? indices?
 
-		(ref, exp, jvf)
+		(oldDef, fixedDef, indices)
 	}
 
 	def writeJSON(data: Seq[(BrokenTDigestAdd, OrderedTDigestAdd, Indices)], fname: String) {
@@ -212,11 +236,13 @@ object AlgebirdFactoryRunner extends App {
 
 
 
-
+	import scala.math.{max, min}
 
 	val (brok, ord, ind): (Seq[Seq[Double]], Seq[Seq[Double]], Seq[Seq[Int]]) = {
 		collect(monoid, new ExponentialDistribution(3.5))
 	}
+	val (xmin, xmax) = (0, 20) // note: number of monoidal additions, get this number out
+	val (ymin, ymax) = (0, max(brok.flatten.max, ord.flatten.max))
 	/*println("broken:")
 	brok.foreach(v => println(v))
 	println("\nordered: ")
@@ -230,17 +256,19 @@ object AlgebirdFactoryRunner extends App {
 
 		def render(plot: Plot, extent: Extent, summary: BoxRenderer.BoxRendererContext): Drawable = ???
 	}*/
-	val bx1 = Some(BoxRenderer.default(Some(fireBrick.lighten(40)), Some(fireBrick), None/*, strokeWidth = Some(10)*/))
-	val bx2 = Some(BoxRenderer.default(Some(dodgerBlue.lighten(40)), Some(dodgerBlue), None/*, Some(3)*/))
+	val bx1 = Some(BoxRenderer.default(Some(red.copy(opacity=0.5)), Some(red), None/*, strokeWidth = Some(10)
+	*/))
+	val bx2 = Some(BoxRenderer.default(Some(dodgerBlue.copy(opacity=0.5)), Some(dodgerBlue), None/*, Some(3)*/))
 
 	displayPlot(
 		Overlay(
 			BoxPlot(data = brok, boxRenderer = bx1),
 			BoxPlot(data =ord, boxRenderer = bx2) //.standard(xLabels = (1 to 10).map(_.toString))
-		).xAxis()
-			.yAxis()
-			.standard(xLabels = (1 to 10).map(_.toString))
-			.frame()
+		)
+			.standard(/*xLabels = (1 to 10).map(_.toString)*/)
+			.xbounds(xmin, xmax)
+			.ybounds(ymin, ymax)
+			//.frame()
 			.render()
 	)
 
