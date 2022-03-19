d6288bca0f1b2721f5bf3950e6167bb1361dfadb
statisticallyfit <statisticallyfit@gmail.com>
2022-03-20 00:46:05 +0200

    finally convert mydist to smiledist todo - must watch for parameters not in the right order?

--- src/test/scala/workspace_snippets/try_Isarn_AddChangingGammas_SMALL.scala
+++ src/test/scala/workspace_snippets/try_Isarn_AddChangingGammas_SMALL.scala
@@ -35,48 +35,51 @@ object try_Isarn_AddChangingGammas_SMALL extends App {
 	val (a1, b1) = (9, 33)
 	val (a2, b2) = (47, 49)
 	val (a3, b3) = (92, 26)
-	val (a4, b4) = (91, 13) // (168, 12) made this crash
+	val (a4, b4) = (83, 13) // (168, 12) made this crash // good (93, 13)
+	val (a5, b5) = (90, 8)
 
 	// Declare the dists https://www.desmos.com/calculator/k5ukou5o1f
 	val greenGamma: GammaDist = GammaDist(a1, b1)
 	val redGamma: GammaDist = GammaDist(a2, b2)
 	val purpleGamma = GammaDist(a3, b3)
 	val orangeGamma = GammaDist(a4, b4)
+	val blueGamma = GammaDist(a5, b5)
 
-	val gammasMovingRight: Seq[GammaDist] = List(greenGamma, redGamma, purpleGamma, orangeGamma)
+	val gammasMovingRight: Seq[GammaDist] = List(greenGamma, redGamma, purpleGamma, orangeGamma, blueGamma)
 
 	// TODO: changed sample size here so total is smaller, not 40,000
-	val gammaData: Seq[Array[Double]] = gammasMovingRight.map(gdist => gdist.sample(1000)) //was 500 working
-	val firstTD: TDigest = TDigest.sketch(gammaData.head, maxDiscrete = MAX_DISCRETE)
+	val gammaDatas: Seq[Array[Double]] = gammasMovingRight.map(gdist => gdist.sample(1000)) //was 500 working
+	val firstTD: TDigest = TDigest.sketch(gammaDatas.head, maxDiscrete = MAX_DISCRETE)
 
 
 	// Creating the sketches and combining them:
-	val shiftedSketch: Seq[TDigest] = gammaData.tail
+	val gammaSketches: Seq[TDigest] = gammaDatas.tail
 		.map((distSmp: Array[Double]) => TDigest.sketch(distSmp, maxDiscrete = MAX_DISCRETE))
 		.scanLeft(firstTD)((ltd: TDigest, rtd: TDigest) => TDigest.combine(ltd, rtd, maxDiscrete = MAX_DISCRETE))
 		.drop(1) // TODO look at algebird factory why erikerlandson drops 1 here
 	// for console
-	// val shiftedSketch: Seq[TDigest] = shiftData.tail.map((distSmp) => TDigest.sketch(distSmp, maxDiscrete = MAX_DISCRETE)).scanLeft(firstTD)((ltd, rtd) => TDigest.combine(ltd, rtd, maxDiscrete = MAX_DISCRETE)).drop(1) // TODO look at algebird factory why erikerlandson drops 1 here
+	// val gammaSketches: Seq[TDigest] = gammaDatas.tail.map((distSmp) => TDigest.sketch(distSmp, maxDiscrete = MAX_DISCRETE)).scanLeft(firstTD)((ltd, rtd) => TDigest.combine(ltd, rtd, maxDiscrete = MAX_DISCRETE)).drop(1) // TODO look at algebird factory why erikerlandson drops 1 here
 
 	// see how convergence is at the last update (after combining the sketches from ALL the distributions)
-	val conceptDriftData = Array.fill[Double](SAMPLE_SIZE){shiftedSketch.last.samplePDF}
+	val conceptDriftData = Array.fill[Double](SAMPLE_SIZE){gammaSketches.last.samplePDF}
 
 	// NOTE FIt the distributions using Mixture model
-	val sampleData: Array[Double] = gammaData.flatten.toArray // just combine the individual datas into one large array
+	val sampleData: Array[Double] = gammaDatas.flatten.toArray // just combine the individual datas into one large array
 
 	// Create the canonical mixture model (against which to compare to the estimated one at the end)
 	// TESTING 1: add same probability to all the gammas
 	// TESTING 2: add increasing probability to the ending gammas
 	// TODO fix the above implicit conversion function (mydist -> smiledist)
-	val a: Mixture.Component = new Mixture.Component(0.25, new GammaDistribution(greenGamma.shape, greenGamma.scale))
-	val b: Mixture.Component = new Mixture.Component(0.25,  new GammaDistribution(redGamma.shape, redGamma.scale))
-	val c: Mixture.Component = new Mixture.Component(0.25, new GammaDistribution(purpleGamma.shape, purpleGamma.scale))
-	val d: Mixture.Component = new Mixture.Component(0.25, new GammaDistribution(orangeGamma.shape, orangeGamma.scale))
-	val canonicalMixture: Mixture = new Mixture(a, b, c,d)
+	val a: Mixture.Component = new Mixture.Component(0.2, new GammaDistribution(greenGamma.shape, greenGamma.scale))
+	val b: Mixture.Component = new Mixture.Component(0.2,  new GammaDistribution(redGamma.shape, redGamma.scale))
+	val c: Mixture.Component = new Mixture.Component(0.2, new GammaDistribution(purpleGamma.shape, purpleGamma.scale))
+	val d: Mixture.Component = new Mixture.Component(0.2, new GammaDistribution(orangeGamma.shape, orangeGamma.scale))
+	val e: Mixture.Component = new Mixture.Component(0.2, new GammaDistribution(blueGamma.shape, blueGamma.scale))
+	val canonicalMixture: Mixture = new Mixture(a, b, c,d, e)
 
 	// Estimate the mixture model
 	// TODO plot the ESTIMATED mixture model
-	val estimatedMixture = ExponentialFamilyMixture.fit(sampleData, a, b, c,d )
+	val estimatedMixture = ExponentialFamilyMixture.fit(sampleData, a, b, c, d, e)
 
 	Console.println(estimatedMixture)
 
@@ -154,21 +157,80 @@ object temp_convertMyDistToSmileDist {
 	}
 
 
-	import scala.reflect.runtime._
-	val cm = universe.runtimeMirror(getClass.getClassLoader)
-	import scala.tools.reflect.ToolBox
-	val tb = cm.mkToolBox()
 
-	val mb = getDistParams(BinomialDist(100, 0.45))
-	//mb: Map[String,(AnyRef, String)] = Map(numTrials -> (100,Integer), p -> (0.45,Double))
-	val (nv, nt) = (mb.get("numTrials").get._1, mb.get("numTrials").get._2)
-	val (pv, pt) = (mb.get("p").get._1, mb.get("p").get._2)
-	val classStr: String = s"BinomialDist($nv.asInstanceOf[$nt], $pv.asInstanceOf[$pt])"
-	val parsed =
-		tb.parse(s"util.distributionExtensions.distributions.BinomialDist($nv.asInstanceOf[$nt], " +
-			s"$pv.asInstanceOf[$pt])")
-	val res = tb.eval(parsed)
 	//TODO now just instead map to the smile distribution one
+	import scala.reflect.runtime.universe._
+
+
+	// USAGE
+	// val res = myDistToSmileDist[IntZ, BinomialDist, BinomialDistribution](myb)
+	//res: smile.stat.distribution.BinomialDistribution = Binomial Distribution(100, 0.4500)
+	//myDistToSmileDist[Real, GammaDist, GammaDistribution](GammaDist(shape = 25.4, scale = 3.1))
+	//res58: smile.stat.distribution.GammaDistribution = Gamma Distribution(3.1000, 25.4000)
+	// NOTE: T must be IntZ / Real not Int / Double TODO fix
+	def myDistToSmileDist[T: Numeric, D, S: TypeTag](distObj: Dist[T, D]): S = {
+		import scala.reflect.runtime._
+		val cm = universe.runtimeMirror(getClass.getClassLoader)
+		import scala.tools.reflect.ToolBox
+		val tb = cm.mkToolBox()
+
+		val paramnameToValAndType: Map[String, (AnyRef, String)] = getDistParams(distObj)
+		//mb: Map[String,(AnyRef, String)] = Map(numTrials -> (100,Integer), p -> (0.45,Double))
+
+		val argStr: String = paramnameToValAndType.foldLeft(""){case (accStr, (paramName, (paramVal, paramType))) => {
+			s"$accStr, $paramVal.asInstanceOf[$paramType]"
+		}}.drop(2) // to drop the first " ,"
+
+		// canonical name smile.stat.distribution.BinomialDistribution
+		val classPckgNameStr: String = typeOf[S].toString
+
+		val classStr: String = s"new $classPckgNameStr($argStr)"
+		// NOTE: must have 'new' since smile's class is not a case class
+		//tb.eval(tb.parse("new smile.stat.distribution.BinomialDistribution(10.asInstanceOf[Int], 0.4.asInstanceOf[Double])"))
+		//res44: Any = Binomial Distribution(10, 0.4000)
+		val parsed: Tree = tb.parse(classStr)
+		val originalDist: S = tb.eval(parsed).asInstanceOf[S] //.asInstanceOf[Dist[T, D]]
+
+		// TODO match up arg names maybe through dict of mydist-names TO smiledist-names (snagit)
+		originalDist
+	}
+
+
+	// USAGE:
+	// scala> reflectDist[Real, BetaDist](BetaDist(4.5, 6.7))
+	// or reflectDist(BetaDist(4.5, 6.7)
+	//res30: util.distributionExtensions.distributions.BetaDist = BetaDist(4.5,6.7)
+	// NOTE: must use my types Real / IntZ not Double / Int WARNING
+	def reflectDist[T: Numeric, D](distObj: Dist[T, D]): D = {
+		import scala.reflect.runtime._
+		val cm = universe.runtimeMirror(getClass.getClassLoader)
+		import scala.tools.reflect.ToolBox
+		val tb = cm.mkToolBox()
+
+		//val mb = getDistParams(BinomialDist(100, 0.45))
+
+		val paramnameToValAndType: Map[String, (AnyRef, String)] = getDistParams(distObj)
+		//mb: Map[String,(AnyRef, String)] = Map(numTrials -> (100,Integer), p -> (0.45,Double))
+
+		/*val (nv, nt) = (paramnameToValAndType.get("numTrials").get._1, paramnameToValAndType.get("numTrials").get._2)
+		val (pv, pt) = (paramnameToValAndType.get("p").get._1, paramnameToValAndType.get("p").get._2)*/
+
+		val argStr: String = paramnameToValAndType.foldLeft(""){case (accStr, (paramName, (paramVal, paramType))) => {
+			s"$accStr, $paramVal.asInstanceOf[$paramType]"
+		}}.drop(2) // to drop the first " ,"
+
+		//val classNameStr: String = distObj.getClass.getSimpleName
+		// canonical name getlso package + file name = util.distributionExtensions.distributions.BinomialDist
+		val classPckgNameStr: String = distObj.getClass.getCanonicalName
+		//val classStr: String = s"$classPckgNameStr($nv.asInstanceOf[$nt], $pv.asInstanceOf[$pt])"
+		val classStr: String = s"$classPckgNameStr($argStr)"
+
+		val parsed: Tree = tb.parse(classStr)
+		val originalDist: D = tb.eval(parsed).asInstanceOf[D] //.asInstanceOf[Dist[T, D]]
+
+		originalDist
+	}
+
 }
 
 
