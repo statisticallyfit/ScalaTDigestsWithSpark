5f9c35dc182592431213cfd72f4b2b4c15b59ba0
statisticallyfit <statisticallyfit@gmail.com>
2022-03-20 17:02:34 +0200

    finished the implicit class conversion from mydist to smiledist and put it in the try_ file

---
+++ src/main/scala/util/ConvertMyDistToSmileDist.scala
@@ -0,0 +1,128 @@
+package util
+
+import util.distributionExtensions.distributions._
+
+import smile.stat.distribution._
+
+import scala.reflect.runtime.universe._
+
+/**
+ *
+ */
+
+
+object ConvertMyDistToSmileDist {
+
+
+	// NOTE: using 'getdeclaredfields' works for my dist[T, D] construction, have to use 'getfields' for the smile dist
+	def getDistParams[T: Numeric, D](dist: Dist[T, D]): Map[String, (AnyRef, String)] = {
+
+		dist.getDist.getClass.getDeclaredFields.toList
+			.map(f => {
+				f.setAccessible(true) //;
+				(f.getName, (f.get(dist.getDist), f.get(dist.getDist).getClass.getSimpleName))
+			}).toMap
+	}
+
+	// D = DiscreteDistribution or ContinuousDistribution (in smile)
+
+	type SmileDist = smile.stat.distribution.Distribution
+
+	// NOTE: using 'getdeclaredfields' works for my dist[T, D] construction, have to use 'getfields' for the smile dist
+	def getSmileDistParams(smileDist: SmileDist): Map[String, (AnyRef, String)] = {
+		smileDist.getClass.getFields.toList
+			.map(f => {
+				f.setAccessible(true)
+				(f.getName, (f.get(smileDist), f.get(smileDist).getClass.getSimpleName))
+			}).toMap
+	}
+
+
+
+	//TODO now just instead map to the smile distribution one
+
+
+	// USAGE
+	// val res = myDistToSmileDist[IntZ, BinomialDist, BinomialDistribution](myb)
+	//res: smile.stat.distribution.BinomialDistribution = Binomial Distribution(100, 0.4500)
+	//myDistToSmileDist[Real, GammaDist, GammaDistribution](GammaDist(shape = 25.4, scale = 3.1))
+	//res58: smile.stat.distribution.GammaDistribution = Gamma Distribution(3.1000, 25.4000)
+	// NOTE: T must be IntZ / Real not Int / Double TODO fix
+	import scala.language.higherKinds
+	import scala.language.implicitConversions
+
+	implicit class ToSmileDist[T: Numeric, D](distObj: Dist[T, D]) {
+
+		def toSmileDist[S: TypeTag]: S = {
+			import scala.reflect.runtime._
+			val cm = universe.runtimeMirror(getClass.getClassLoader)
+			import scala.tools.reflect.ToolBox
+			val tb = cm.mkToolBox()
+
+			val paramnameToValAndType: Map[String, (AnyRef, String)] = getDistParams(distObj)
+			//mb: Map[String,(AnyRef, String)] = Map(numTrials -> (100,Integer), p -> (0.45,Double))
+
+			val argStr: String = paramnameToValAndType.foldLeft("") { case (accStr, (paramName, (paramVal, paramType))) => {
+				s"$accStr, $paramVal.asInstanceOf[$paramType]"
+			}}.drop(2) // to drop the first " ,"
+
+			// canonical name smile.stat.distribution.BinomialDistribution
+			val classPckgNameStr: String = typeOf[S].toString
+
+			val classStr: String = s"new $classPckgNameStr($argStr)"
+			// NOTE: must have 'new' since smile's class is not a case class
+			//tb.eval(tb.parse("new smile.stat.distribution.BinomialDistribution(10.asInstanceOf[Int], 0.4.asInstanceOf[Double])"))
+			//res44: Any = Binomial Distribution(10, 0.4000)
+			val parsed: Tree = tb.parse(classStr)
+			val originalDist: S = tb.eval(parsed).asInstanceOf[S] //.asInstanceOf[Dist[T, D]]
+
+			// TODO match up arg names maybe through dict of mydist-names TO smiledist-names (snagit)
+			originalDist
+		}
+	}
+
+
+	//def toSmiledist[T: Numeric, D, S: TypeTag](distObj: Dist[T, D]): S =
+	// Some Source code = https://stackoverflow.com/a/59894917
+
+
+
+
+
+	// USAGE:
+	// scala> reflectDist[Real, BetaDist](BetaDist(4.5, 6.7))
+	// or reflectDist(BetaDist(4.5, 6.7)
+	//res30: util.distributionExtensions.distributions.BetaDist = BetaDist(4.5,6.7)
+	// NOTE: must use my types Real / IntZ not Double / Int WARNING
+	def reflectDist[T: Numeric, D](distObj: Dist[T, D]): D = {
+		import scala.reflect.runtime._
+		val cm = universe.runtimeMirror(getClass.getClassLoader)
+		import scala.tools.reflect.ToolBox
+		val tb = cm.mkToolBox()
+
+		//val mb = getDistParams(BinomialDist(100, 0.45))
+
+		val paramnameToValAndType: Map[String, (AnyRef, String)] = getDistParams(distObj)
+		//mb: Map[String,(AnyRef, String)] = Map(numTrials -> (100,Integer), p -> (0.45,Double))
+
+		/*val (nv, nt) = (paramnameToValAndType.get("numTrials").get._1, paramnameToValAndType.get("numTrials").get._2)
+		val (pv, pt) = (paramnameToValAndType.get("p").get._1, paramnameToValAndType.get("p").get._2)*/
+
+		val argStr: String = paramnameToValAndType.foldLeft("") { case (accStr, (paramName, (paramVal, paramType))) => {
+			s"$accStr, $paramVal.asInstanceOf[$paramType]"
+		}
+		}.drop(2) // to drop the first " ,"
+
+		//val classNameStr: String = distObj.getClass.getSimpleName
+		// canonical name getlso package + file name = util.distributionExtensions.distributions.BinomialDist
+		val classPckgNameStr: String = distObj.getClass.getCanonicalName
+		//val classStr: String = s"$classPckgNameStr($nv.asInstanceOf[$nt], $pv.asInstanceOf[$pt])"
+		val classStr: String = s"$classPckgNameStr($argStr)"
+
+		val parsed: Tree = tb.parse(classStr)
+		val originalDist: D = tb.eval(parsed).asInstanceOf[D] //.asInstanceOf[Dist[T, D]]
+
+		originalDist
+	}
+	// TODO similar = http://www.smartjava.org/content/scala-typeclass-explained-implement-stringread-function/
+}
--- src/test/scala/workspace_snippets/try_Isarn_AddChangingGammas_SMALL.scala
+++ src/test/scala/workspace_snippets/try_Isarn_AddChangingGammas_SMALL.scala
@@ -8,6 +8,8 @@ import util.distributionExtensions.distributions._
 import util.distributionExtensions.instances._
 import util.distributionExtensions.syntax._
 
+import util.ConvertMyDistToSmileDist._
+import scala.language.implicitConversions
 
 import com.cibo.evilplot._
 import com.cibo.evilplot.colors.{Color, HTMLNamedColors}
@@ -70,12 +72,13 @@ object try_Isarn_AddChangingGammas_SMALL extends App {
 	// TESTING 1: add same probability to all the gammas
 	// TESTING 2: add increasing probability to the ending gammas
 	// TODO fix the above implicit conversion function (mydist -> smiledist)
-	val a: Mixture.Component = new Mixture.Component(0.2, new GammaDistribution(greenGamma.shape, greenGamma.scale))
-	val b: Mixture.Component = new Mixture.Component(0.2,  new GammaDistribution(redGamma.shape, redGamma.scale))
-	val c: Mixture.Component = new Mixture.Component(0.2, new GammaDistribution(purpleGamma.shape, purpleGamma.scale))
-	val d: Mixture.Component = new Mixture.Component(0.2, new GammaDistribution(orangeGamma.shape, orangeGamma.scale))
-	val e: Mixture.Component = new Mixture.Component(0.2, new GammaDistribution(blueGamma.shape, blueGamma.scale))
-	val canonicalMixture: Mixture = new Mixture(a, b, c,d, e)
+	/*val a: Mixture.Component = new Mixture.Component(0.2, new GammaDistribution(greenGamma.shape, greenGamma.scale))*/
+	val a: Mixture.Component = new Mixture.Component(0.2, greenGamma.toSmileDist[GammaDistribution])
+	val b: Mixture.Component = new Mixture.Component(0.2, redGamma.toSmileDist[GammaDistribution])
+	val c: Mixture.Component = new Mixture.Component(0.2, purpleGamma.toSmileDist[GammaDistribution])
+	val d: Mixture.Component = new Mixture.Component(0.2, orangeGamma.toSmileDist[GammaDistribution])
+	val e: Mixture.Component = new Mixture.Component(0.2, blueGamma.toSmileDist[GammaDistribution])
+	val canonicalMixture: Mixture = new Mixture(a, b, c, d, e)
 
 	// Estimate the mixture model
 	// TODO plot the ESTIMATED mixture model
@@ -100,161 +103,4 @@ object try_Isarn_AddChangingGammas_SMALL extends App {
 	plotMixtureDist(sampleData, canonicalMixture, estimatedMixture, k)
 
 
-
-
-	// TODO how to make the plot take the linspace of x values, to control the granularity of plotted values?
-	/*import breeze.linalg.linspace
-	val NUM_POINTS: Int = 2000
-	val xs: Seq[Double] = linspace(xMIN, xMAX, length = NUM_POINTS).toScalaVector //(xMIN to xMAX).toList
-	val probsCanonicalMixture: Seq[Double] = xs.map(x => canonicalMixture.p(x))
-	val probsEstimatedMixture: Seq[Double] = xs.map(x => estimatedMixture.p(x))
-	val probsKernel: Seq[Double] = xs.map(x => k.p(x))*/
-
-	/*val makeCanonicalMixture: Double => Double = x => canonicalMixture.p(x)
-	val makeEstimatedMixture: Double => Double = x => estimatedMixture.p(x)
-	val makeKernel: Double => Double = x => k.p(x)*/
-
-
-
-
 }
-// TODO maybe use scala.toolbox with string interpolation of classnames and parameters?
-
-object temp_convertMyDistToSmileDist {
-
-	/*def getDistParams[T: Numeric, D](dist: Dist[T, D]): List[String] = {
-
-		dist.getDist.getClass.getDeclaredFields.toList
-			.map(f => {f.setAccessible(true) ; f.getName})
-	}*/
-
-	/*import scala.reflect.runtime.universe._
-
-	def inspect[T: TypeTag](x: T): String = typeOf[T].toString*/
-
-
-
-	// NOTE: using 'getdeclaredfields' works for my dist[T, D] construction, have to use 'getfields' for the smile dist
-	def getDistParams[T: Numeric, D](dist: Dist[T, D]): Map[String, (AnyRef, String)] = {
-
-		dist.getDist.getClass.getDeclaredFields.toList
-			.map(f => {
-				f.setAccessible(true) //;
-				(f.getName, (f.get(dist.getDist), f.get(dist.getDist).getClass.getSimpleName))
-			}).toMap
-	}
-	// NOTE: using 'getdeclaredfields' works for my dist[T, D] construction, have to use 'getfields' for the smile dist
-	// D = DiscreteDistribution or ContinuousDistribution (in smile)
-
-	type SmileDist = smile.stat.distribution.Distribution
-
-	def getSmileDistParams(smileDist: SmileDist): Map[String, (AnyRef, String)] = {
-		smileDist.getClass.getFields.toList
-			.map(f => {
-				f.setAccessible(true)
-				(f.getName, (f.get(smileDist), f.get(smileDist).getClass.getSimpleName ))
-			}).toMap
-	}
-
-
-
-	//TODO now just instead map to the smile distribution one
-	import scala.reflect.runtime.universe._
-
-
-	// USAGE
-	// val res = myDistToSmileDist[IntZ, BinomialDist, BinomialDistribution](myb)
-	//res: smile.stat.distribution.BinomialDistribution = Binomial Distribution(100, 0.4500)
-	//myDistToSmileDist[Real, GammaDist, GammaDistribution](GammaDist(shape = 25.4, scale = 3.1))
-	//res58: smile.stat.distribution.GammaDistribution = Gamma Distribution(3.1000, 25.4000)
-	// NOTE: T must be IntZ / Real not Int / Double TODO fix
-	def myDistToSmileDist[T: Numeric, D, S: TypeTag](distObj: Dist[T, D]): S = {
-		import scala.reflect.runtime._
-		val cm = universe.runtimeMirror(getClass.getClassLoader)
-		import scala.tools.reflect.ToolBox
-		val tb = cm.mkToolBox()
-
-		val paramnameToValAndType: Map[String, (AnyRef, String)] = getDistParams(distObj)
-		//mb: Map[String,(AnyRef, String)] = Map(numTrials -> (100,Integer), p -> (0.45,Double))
-
-		val argStr: String = paramnameToValAndType.foldLeft(""){case (accStr, (paramName, (paramVal, paramType))) => {
-			s"$accStr, $paramVal.asInstanceOf[$paramType]"
-		}}.drop(2) // to drop the first " ,"
-
-		// canonical name smile.stat.distribution.BinomialDistribution
-		val classPckgNameStr: String = typeOf[S].toString
-
-		val classStr: String = s"new $classPckgNameStr($argStr)"
-		// NOTE: must have 'new' since smile's class is not a case class
-		//tb.eval(tb.parse("new smile.stat.distribution.BinomialDistribution(10.asInstanceOf[Int], 0.4.asInstanceOf[Double])"))
-		//res44: Any = Binomial Distribution(10, 0.4000)
-		val parsed: Tree = tb.parse(classStr)
-		val originalDist: S = tb.eval(parsed).asInstanceOf[S] //.asInstanceOf[Dist[T, D]]
-
-		// TODO match up arg names maybe through dict of mydist-names TO smiledist-names (snagit)
-		originalDist
-	}
-
-
-	// USAGE:
-	// scala> reflectDist[Real, BetaDist](BetaDist(4.5, 6.7))
-	// or reflectDist(BetaDist(4.5, 6.7)
-	//res30: util.distributionExtensions.distributions.BetaDist = BetaDist(4.5,6.7)
-	// NOTE: must use my types Real / IntZ not Double / Int WARNING
-	def reflectDist[T: Numeric, D](distObj: Dist[T, D]): D = {
-		import scala.reflect.runtime._
-		val cm = universe.runtimeMirror(getClass.getClassLoader)
-		import scala.tools.reflect.ToolBox
-		val tb = cm.mkToolBox()
-
-		//val mb = getDistParams(BinomialDist(100, 0.45))
-
-		val paramnameToValAndType: Map[String, (AnyRef, String)] = getDistParams(distObj)
-		//mb: Map[String,(AnyRef, String)] = Map(numTrials -> (100,Integer), p -> (0.45,Double))
-
-		/*val (nv, nt) = (paramnameToValAndType.get("numTrials").get._1, paramnameToValAndType.get("numTrials").get._2)
-		val (pv, pt) = (paramnameToValAndType.get("p").get._1, paramnameToValAndType.get("p").get._2)*/
-
-		val argStr: String = paramnameToValAndType.foldLeft(""){case (accStr, (paramName, (paramVal, paramType))) => {
-			s"$accStr, $paramVal.asInstanceOf[$paramType]"
-		}}.drop(2) // to drop the first " ,"
-
-		//val classNameStr: String = distObj.getClass.getSimpleName
-		// canonical name getlso package + file name = util.distributionExtensions.distributions.BinomialDist
-		val classPckgNameStr: String = distObj.getClass.getCanonicalName
-		//val classStr: String = s"$classPckgNameStr($nv.asInstanceOf[$nt], $pv.asInstanceOf[$pt])"
-		val classStr: String = s"$classPckgNameStr($argStr)"
-
-		val parsed: Tree = tb.parse(classStr)
-		val originalDist: D = tb.eval(parsed).asInstanceOf[D] //.asInstanceOf[Dist[T, D]]
-
-		originalDist
-	}
-
-}
-
-
-// Source code = https://stackoverflow.com/a/59894917
-/*implicit def convertMyDistToSmileDist[T: Numeric, D](dist: Dist[T, D]): smile.stat.distribution.Distribution = {
-	val obj: D = dist.getDist
-	//obj.getClass.getConstructors
-	//obj.getClass.getDeclaredFields.toList
-	// TODO: when using my object (in distributionExtensions package), the `getDeclaredFields` restricts to
-	//  returning only the fields of the class and not the superclass, whereas inverse is true for the smile's
-	//  distributions --- WHY?
-	// NOTE: temporaroy solution = use `getDeclaredFields` when operating on distributionExtensions and use
-	//  `getFields` when operating on smile's dists.
-	obj.getClass.getDeclaredFields
-	/*val args: List[(String, AnyRef)] = obj.getClass.getFields.toList
-		.map(f => {
-			f.setAccessible(true)
-			f.getName -> f.get(obj)
-	})*/
-
-	new GammaDistribution(args:_*).
-	//TODO methods of doing it:
-	1. create manual map from my.dist to smile.dist
-	2. above arbitrary gett of arguments (but then problem of how to put them since can't pass arbitrary number
-	 when expecting a particular number in the constructor...)
-
-}*/
